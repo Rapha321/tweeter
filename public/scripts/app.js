a9d4b1e72565ccb8de8ddbd4315358f6051d7dc8
Rapha321 <ralpha19@hotmail.com>
2018-12-14 20:55:13 +0000

    fixed bugs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,113 +1,20 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": {
-  //       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // }
 
 
-// function createTweetElement(obj) {
-
-//   return `
-//     <article class="indiv-tweets">
-//       <header>
-//         <img class="image" src=${obj.user.avatars.small}>
-//         <span class="name">${obj.user.name}</span>
-//         <span class="handle">${obj.user.handle}</span>
-//       </header>
-
-//       <div class="body">
-//         <p>${obj.content.text}</p>
-//       </div>
-
-//       <footer>
-//         <span>${obj.created_at}</span>
-//         <div class="animation">
-//           <i class="fa fa-flag" aria-hidden="true"></i>
-//           <i class="fa fa-retweet" aria-hidden="true"></i>
-//           <i class="fa fa-heart" aria-hidden="true"></i>
-//         </div>
-//       </footer>
-//     </article>
-// `;
-// }
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
-
+// added cross-site scripting to avoid evaluating text from untrusted sources
 function escape(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-
+// loop through tweet database and prepend to original tweet
 function renderTweets(tweets) {
   for (let idiv of tweets) {
     $("#unique").prepend(createTweetElement(idiv));
   }
 }
 
+// setting the format of how tweet box should looks like
 function createTweetElement(obj) {
   let article = `<article class="indiv-tweets">
       <header>
@@ -129,25 +36,12 @@ function createTweetElement(obj) {
         </div>
       </footer>
     </article>`
-
   let $tweet = $(article).addClass('tweet');
   return $tweet;
 }
 
-// $(document).ready(function() {
-//   renderTweets(data);
-// });
-
-
-// $(document).ready(function() {
-//   var $tweet = createTweetElement(tweetData);
-//   console.log($tweet)
-
-//   $(".container").append($tweet);
-// });
-
+// this will make sure that
 $(document).ready(function() {
-
   let loadTweets = function() {
     $.ajax("/tweets", {method: 'GET'})
     .then(function (tweets) {
@@ -186,6 +80,7 @@ $(document).ready(function() {
       return true;
     }
   }
+
 
   $( '.new-tweet' ).hide();
   $( '.large-button' ).click(function() {
